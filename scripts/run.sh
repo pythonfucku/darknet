fd3a11d27f7268a499e82d834637fa25756f2652
pythonfucku <lrt_no1@163.com>
2018-07-31 13:56:58 +0800

    update

---
+++ scripts/run.sh
@@ -0,0 +1,5 @@
+#python reval_voc.py --year 2012 --classes /home/crow/workspace/BBA/aiDATA/a_image_VOC_factory/VOCdevkit/sq/cfg/voc.names --image_set test --voc_dir ~/workspace/BBA/aiDATA/a_image_VOC_factory/VOCdevkit/sq /home/crow/workspace/BBA/ai/src/liangrentao/darknet/results
+
+
+
+python reval_voc.py --year 2002 --classes /home/crow/workspace/BBA/aiDATA/a_image_VOC_factory/VOCdevkit/sq-DGX/3/cfg/voc.names --image_set test --voc_dir ~/workspace/BBA/aiDATA/a_image_VOC_factory/VOCdevkit/sq-DGX /home/crow/workspace/BBA/ai/src/liangrentao/darknet/results
